9054839fa536a352721f26e9dd6d8d6cea4141ce
sourabh gupta <KausTiVoKumar@tivo.com>
2019-06-24 23:01:30 +0530

    docker sample project

---
+++ src/main/java/com/acheron/transcoder/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.acheron.transcoder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@RequestMapping("/")
+    public String hello() {
+            return "Hello Spring Boot!";
+    }
+}
